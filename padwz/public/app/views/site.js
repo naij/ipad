5519b437c67e85e9f1ed30ae99d533b891bb0e42
wolongxzg <wolongxzg@gmail.com>
2012-09-19 08:49:08 +0800

    add slide

--- padwz/public/app/views/site.js
+++ padwz/public/app/views/site.js
@@ -7,11 +7,6 @@ define(function(require, exports, module) {
 
         template: _.template(siteViewTemplate),
 
-        events: { 
-            'swipeLeft .main' : 'swipeLeft',
-            'swipeRight .main' : 'swipeRight'
-        },
-
         initialize: function(options){
             this.render();  
         }, 
@@ -20,31 +15,14 @@ define(function(require, exports, module) {
             $(this.el).html(this.template());
 
             var site = $(this.el).find('.site');
-            var siteBd = $(this.el).find('.site-bd');
-            var panel = $(this.el).find('.panel');
-            var width = panel.width();
-            var height = panel.height();
-
-            site.css({'width':width + 'px','height':height +'px'});
-            panel.css('width',width + 'px');
-            siteBd.css('width',width*2 + 'px');
-        },
-
-        swipeLeft : function(e){
-            var siteBd = $(this.el).find('.site-bd');
             var panel = $(this.el).find('.panel');
             var width = panel.width();
 
-            siteBd.anim({translateX: '-'+ width +'px'}, 0.45, 'cubic-bezier(0, 0, 0.1, 1.0)');
-        },
+            site.css({'width':width + 'px'});
 
-        swipeRight : function(e){
-            var siteBd = $(this.el).find('.site-bd');
-            var panel = $(this.el).find('.panel');
-            var width = panel.width();
-
-            siteBd.anim({translateX: '0px'}, 0.45, 'cubic-bezier(0, 0, 0.1, 1.0)');
-        }  
+            //水平滑动
+            var slider = new Swipe($('#site')[0]);
+        }
     });
 
     return siteView;
